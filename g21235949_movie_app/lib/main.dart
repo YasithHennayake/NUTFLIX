8568f98e2af63e37d8f433968a1182ae63b3f3a7
Yasith Hennayake <yasith.ghosty@gmail.com>
2024-02-17 19:31:38 +0530

    Setup Firebase by Adding configuration files for Web,Android and iOS

--- g21235949_movie_app/lib/main.dart
+++ g21235949_movie_app/lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDVu7C0g3VjQO43u1flaeHHOKOxAxBocdA",
+            appId: "1:954112658260:web:23d870c8b1c912e5b596c1",
+            messagingSenderId: "954112658260",
+            projectId: "nutflix-b4527"));
+  }
+
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
